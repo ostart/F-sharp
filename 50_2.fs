6f5cd74ddaa603c6ef57b5390a3a582b1ce64032
Artem Osta <ostart@mail.ru>
2019-06-27 06:52:42 +0300

    Task 50.2 corrected

--- 50_2.fs
+++ 50_2.fs
@@ -5,17 +5,30 @@ let rec factorial n =
 
 // 50.2.1
 let fac_seq = seq {
-    let mutable i = 0
-    while true do
+    for i in 0..System.Int32.MaxValue do
       yield (factorial i)
-      i <- i + 1
        }
 
 // 50.2.2
 let seq_seq = seq {
     yield 0
-    let mutable i = 1
-    while true do
+    for i in 1..System.Int32.MaxValue do
       yield! seq [-i;i]
-      i <- i + 1
        }
+
+// // 50.2.1
+// let fac_seq = seq {
+//     let mutable i = 0
+//     while true do
+//       yield (factorial i)
+//       i <- i + 1
+//        }
+
+// // 50.2.2
+// let seq_seq = seq {
+//     yield 0
+//     let mutable i = 1
+//     while true do
+//       yield! seq [-i;i]
+//       i <- i + 1
+//        }
