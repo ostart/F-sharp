a06dd90953a312cd08b17756234a1c6fd8a371ed
IKomarovLeonid <49585534+IKomarovLeonid@users.noreply.github.com>
2020-02-26 16:01:05 +0300

    Update 49_4.fs

--- 49_4.fs
+++ 49_4.fs
@@ -1,19 +1,16 @@
-let rec factorial n =
-    match n with
-    | 0 | 1 -> 1
-    | _ -> n * factorial(n-1)
+let even_seq = Seq.initInfinite (fun i -> (2*i+2)) 
 
-let rec alternate n =
-    if(n=0) then 0
-    else
-      if((n%2)=0) then alternate(n-1) + n
-      else alternate(n-1) - n
+let factorial n =
+    let rec f x a =
+        if x <= 1 then a
+        else f (x - 1) (a * x)
+    f n 1
 
-// 49.5.1
-let even_seq = Seq.initInfinite (fun i -> 2*i + 2)
+let fac_seq = Seq.initInfinite factorial 
 
-// 49.5.2
-let fac_seq = Seq.initInfinite (factorial)
+let check_num n = 
+    if n = 0 then 0
+    elif n % 2 = 0 then (n / 2)
+    else n / 2 - n
 
-// 49.5.3
-let seq_seq = Seq.initInfinite(alternate)
+let seq_seq = Seq.initInfinite check_num
